792c9b8d06ac9da78888e3d0d366d51d3552d472
Ken Chen <lbkchen@gmail.com>
2017-07-06 13:44:10 -0700

    Configure username params for members.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :mirrors
-  devise_for :members
+  devise_for :members, controllers: {
+    passwords: 'members/passwords',
+    registrations: 'members/registrations'
+  }
   get 'home/index'
   root 'home#index'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :mirrors do
     resources :members
